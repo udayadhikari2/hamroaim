270bef793425ba6026712dfa92af455f7412d384
uday <uday.adhikari2@gmail.com>
2021-09-27 22:57:10 +0545

    error

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,21 +1,21 @@
 import React, { useRef } from "react";
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
-import Layout from "../components/Layout";
-import Footer from "../components/Footer/Footer";
+// import Layout from "@/components/Layout";
+// import Footer from "@/components/Footer";
 
 function MyApp({ Component, pageProps }) {
   const scrollRef = useRef(null);
   return (
     <ChakraProvider>
       <div className="w-full h-screen flex flex-col sm:flex-row overflow-hidden">
-        <Layout scrollRef={scrollRef} />
+        {/* <Layout scrollRef={scrollRef} /> */}
         <div
           ref={scrollRef}
           className="w-screen h-screen overflow-x-hidden overflow-y-auto"
         >
           <Component {...pageProps} />
-          <Footer />
+          {/* <Footer /> */}
         </div>
       </div>
     </ChakraProvider>
@@ -23,13 +23,3 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
-
-{
-  /* <div className="w-full h-screen flex flex-col sm:flex-row overflow-hidden">
-<NavBar scrolled={scrolled} setScrolled={setScrolled} />
-<div
-  ref={scrollRef}
-  className="w-screen h-screen overflow-x-hidden overflow-y-auto"
-></div>
-</div> */
-}
