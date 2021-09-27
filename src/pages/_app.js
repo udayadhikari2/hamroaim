a17a82d097b9ef6036f57342b7590f793b130684
aashish rana magar <gorkhali9999@gmail.com>
2021-09-27 23:01:12 +0545

    solved issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,21 +1,21 @@
 import React, { useRef } from "react";
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
-// import Layout from "@/components/Layout";
-// import Footer from "@/components/Footer";
+import Layout from "@/components/Layout";
+import Footer from "@/components/Footer";
 
 function MyApp({ Component, pageProps }) {
   const scrollRef = useRef(null);
   return (
     <ChakraProvider>
       <div className="w-full h-screen flex flex-col sm:flex-row overflow-hidden">
-        {/* <Layout scrollRef={scrollRef} /> */}
+        <Layout scrollRef={scrollRef} />
         <div
           ref={scrollRef}
           className="w-screen h-screen overflow-x-hidden overflow-y-auto"
         >
           <Component {...pageProps} />
-          {/* <Footer /> */}
+          <Footer />
         </div>
       </div>
     </ChakraProvider>
